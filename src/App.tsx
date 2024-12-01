be1d40f5179b07ec91885a9eab3aa7d7c4d75a48
igor-nikitin-cc <168405829+igor-nikitin-cc@users.noreply.github.com>
2024-12-01 11:32:56 +0300

    base commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import logo from './assets/up_306.jpg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={logo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
